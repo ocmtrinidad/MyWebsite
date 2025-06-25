f6ce08b809154838e35168cfaf12ce3b865563aa
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-06-25 09:51:47 +0800

    Updated

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,17 +1,18 @@
 <?php 
 
-// Data Source Name (dsn).
 // Using localhost as we are using XAMPP local server. Use hosting provider info for online server.
-$dsn = "mysql:host=localhost;dbname=myfirstdatabase";
+$host = "localhost";
+$dbname = "myfirstdatabase";
 // Default XAMPP username is root & password is "". For mac users, default password is "root".
 $dbusername = "root";
 $dbpassword = "";
 
 try {
   // new PDO (Php Data Objects). Creates an object that connects to database.
-  $pdo = new PDO($dsn, $dbusername, $dbpassword);
-  // Error handling. Uses setAttribute() from pdo object
+  // new PDO(Data Source Name, database username, database password).
+  $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
+  // For allowing error handling.
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-};
+  die("Connection failed: " . $e->getMessage());
+}
